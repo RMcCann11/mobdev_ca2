18444154db7a9f1a239bfe8c4751c5ace5dc7a9a
RMcCann11 <ronanemccann@hotmail.com>
2022-11-04 16:56:31 +0000

    spaceships.pages.ts changed to create an Observable to make API calls

--- src/app/pages/spaceships/spaceships.page.ts
+++ src/app/pages/spaceships/spaceships.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-spaceships',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpaceshipsPage implements OnInit {
 
-  constructor() { }
+  spaceships : Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.spaceships = this.http.get('https://swapi.dev/api/starships');
   }
+
+  openDetails(spaceship) {
+    let split = spaceship.url.split('/');
+    let spaceshipId = split[split.length-2];
+    this.router.navigateByUrl(`/tabs/starships/${spaceshipId}`);
+    }
 
 }
